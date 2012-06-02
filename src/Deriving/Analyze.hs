982682a9f29626c949f6d29672f9ffff51daedee
Nikolaos Bezirgiannis <bezirg@gmail.com>
2012-06-02 19:56:23 +0200

    Deriving

--- src/Deriving/Analyze.hs
+++ src/Deriving/Analyze.hs
@@ -16,50 +16,158 @@ analyzeModule _ = mempty
 
 analyzeDecl :: Decl -> Analysis
 
--- data or newtype
-analyzeDecl (DataDecl _ dataOrNew _ _ name _ ds) = let lds = length ds 
-                                                       nds = map derivingName ds
-                                                   in 
-                                                     Analysis  
-                                                     lds -- size
-                                                     (M.fromList $ zip nds (repeat 1)) -- top
-                                                     lds -- normal
-                                                     0   -- standalone
-                                                     (if dataOrNew == NewType then length (nonGNDClasses `intersect` nds) else 0) -- newtype
-                                                     0 -- overload
-                                                     M.empty
+-- NORMAL
+---------
 
+-- data
+analyzeDecl (DataDecl _ DataType _ _ name _ ds) = let lds = length ds 
+                                                      nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , normalStruct = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , normalPos = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , normalDer = lds
+                                                          , topDer = (M.fromList $ zip nds (repeat 1))
+                                                          }
+-- ok
 
--- gadt data or new type
-analyzeDecl (GDataDecl _ dataOrNew _ name _ _ _ ds) = analyzeDecl (DataDecl undefined dataOrNew undefined undefined undefined undefined ds) -- same as DataDecl
+-- newtype
+analyzeDecl (DataDecl _ NewType _ _ name _ ds) = let lds = length ds 
+                                                     nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , normalStruct = 1
+                                                          , newtypeStructContains = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , normalPos = (fromBool $ not $ null ds)
+                                                          , newtypePosContains = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , normalDer = lds
+                                                          , newtypeDerContains = lds
+                                                          }
 
--- data or newtype families
-analyzeDecl (DataInsDecl _ dataOrNew _  _ ds) = analyzeDecl (DataDecl undefined dataOrNew undefined undefined undefined undefined ds) -- same as DataDecl
+-- ok
 
--- gadt data or newtype families
-analyzeDecl (GDataInsDecl _ dataOrNew typeParams _ _ ds) = analyzeDecl (DataDecl undefined dataOrNew undefined undefined undefined undefined ds) -- same as DataDecl
+-- GADT
+-------
+
+-- data
+analyzeDecl (GDataDecl _ DataType _ name _ _ _ ds) = let lds = length ds 
+                                                         nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , gadtStruct = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , gadtPos = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , gadtDer = lds
+                                                          , topDer = (M.fromList $ zip nds (repeat 1))
+                                                          }
+
+-- ok
+
+-- newtype
+analyzeDecl (GDataDecl _ NewType _ name _ _ _ ds) = let lds = length ds 
+                                                        nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , gadtStruct = 1
+                                                          , newtypeStructContains = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , gadtPos = (fromBool $ not $ null ds)
+                                                          , newtypePosContains = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , gadtDer = lds
+                                                          , newtypeDerContains = lds
+                                                          }
+-- ok
+
+-- FAMSTRUCT
+------------
+
+-- data
+analyzeDecl (DataInsDecl _ DataType _  _ ds) = let lds = length ds 
+                                                   nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , famStruct = 1
+                                                          , newtypeStructContains = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , famPos = (fromBool $ not $ null ds)
+                                                          , newtypePosContains = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , famDer = lds
+                                                          , newtypeDerContains = lds
+                                                          }
+
+-- ok
+
+-- newtype
+analyzeDecl (DataInsDecl _ NewType _  _ ds) = let lds = length ds 
+                                                  nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , famStruct = 1
+                                                          , newtypeStructContains = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , famPos = (fromBool $ not $ null ds)
+                                                          , newtypePosContains = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , famDer = lds
+                                                          , newtypeDerContains = lds
+                                                          }
+
+-- ok
+
+-- GADTFAMSTRUCT
+----------------
+
+-- data
+analyzeDecl (GDataInsDecl _ DataType typeParams _ _ ds) = let lds = length ds 
+                                                              nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , gadtFamStruct = 1
+                                                          , newtypeStructContains = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , gadtFamPos = (fromBool $ not $ null ds)
+                                                          , newtypePosContains = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , gadtFamDer = lds
+                                                          , newtypeDerContains = lds
+                                                          }
+
+-- ok
+
+-- newtype
+analyzeDecl (GDataInsDecl _ NewType typeParams _ _ ds) = let lds = length ds 
+                                                             nds = map derivingName ds
+                                                   in mempty {
+                                                            sizeStruct = 1
+                                                          , gadtFamStruct = 1
+                                                          , newtypeStructContains = 1
+                                                          , sizePos = (fromBool $ not $ null ds)
+                                                          , gadtFamPos = (fromBool $ not $ null ds)
+                                                          , newtypePosContains = (fromBool $ not $ null ds)
+                                                          , sizeDer = lds
+                                                          , gadtFamDer = lds
+                                                          , newtypeDerContains = lds
+                                                          }
+
+-- ok
 
 -- standalone deriving
-analyzeDecl (DerivDecl _ _ derClass typeParams) = let isDeriveableInstance = qNameToString derClass `elem` deriveableClasses
-                                                   in
-                                                     Analysis 
-                                                     1 -- size
-                                                     (M.singleton (qNameToString derClass) 1) -- top
-                                                     0 -- normal
-                                                     1 -- standalone
-                                                     (fromBool $ not isDeriveableInstance) -- newtype
-                                                     0 -- overload
-                                                     M.empty
+analyzeDecl (DerivDecl _ _ derClass typeParams) = mempty { stdAloneDecl = 1}
 
 analyzeDecl (InstDecl _ _ derClass typeParams _) = let isDeriveableInstance = qNameToString derClass `elem` deriveableClasses
-                                                   in
-                                                     Analysis 
-                                                     0 -- size
-                                                     M.empty -- top
-                                                     0       -- normal
-                                                     0       -- standalone
-                                                     0       -- newtype
-                                                     (fromBool isDeriveableInstance)  -- overload
-                                                     (if isDeriveableInstance then M.singleton (qNameToString derClass) 1 else M.empty)
-
+                                                   in mempty {
+                                                            sizeInst = 1
+                                                          , overloadInst = fromBool isDeriveableInstance
+                                                          , topOverload = if isDeriveableInstance 
+                                                                          then M.singleton (qNameToString derClass) 1 
+                                                                          else M.empty
+                                                          }
+                                                                      
 analyzeDecl _ = mempty
--- src/Deriving/Base.hs
+++ src/Deriving/Base.hs
@@ -7,36 +7,84 @@ import qualified Data.Map as M
 import Data.Monoid
 
 data Analysis = Analysis {
-      sizeDer :: Int
-    , topDer :: M.Map ClassName Int
+    sizeStruct :: Int
+    , normalStruct :: Int
+    , gadtStruct :: Int
+    , famStruct :: Int
+    , gadtFamStruct :: Int
+    , newtypeStructContains :: Int
+    , sizePos :: Int
+    , normalPos :: Int
+    , gadtPos :: Int
+    , famPos :: Int
+    , gadtFamPos :: Int
+    , newtypePosContains :: Int
+    , sizeDer :: Int
     , normalDer :: Int
-    , stdAloneDer :: Int
-    , newtypeDer :: Int
+    , gadtDer :: Int
+    , famDer :: Int
+    , gadtFamDer :: Int
+    , newtypeDerContains :: Int
+    , stdAloneDecl :: Int         -- == stdAloneStruct == stdAlonePos
+    , topDer :: M.Map ClassName Int
+    , sizeInst :: Int
     , overloadInst :: Int
     , topOverload :: M.Map ClassName Int
     }
               deriving (Show)
 
 instance Monoid Analysis where
-    mempty = Analysis 0 M.empty 0 0 0 0 M.empty
-    (Analysis x1 x2 x3 x4 x5 x6 x7) `mappend` (Analysis y1 y2 y3 y4 y5 y6 y7) = Analysis
+    mempty = Analysis 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 mempty 0 0 mempty
+    (Analysis x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13 x14 x15 x16 x17 x18 x19 x20 x21 x22 x23) `mappend` (Analysis y1 y2 y3 y4 y5 y6 y7 y8 y9 y10 y11 y12 y13 y14 y15 y16 y17 y18 y19 y20 y21 y22 y23) = Analysis
                                                                                             (x1+y1)
-                                                                                            (M.unionWith (+) x2 y2)
+                                                                                            (x2+y2)
                                                                                             (x3+y3)
                                                                                             (x4+y4)
                                                                                             (x5+y5)
                                                                                             (x6+y6)
-                                                                                            (M.unionWith (+) x7 y7)
+                                                                                            (x7+y7)
+                                                                                            (x8+y8)
+                                                                                            (x9+y9)
+                                                                                            (x10+y10)
+                                                                                            (x11+y11)
+                                                                                            (x12+y12)
+                                                                                            (x13+y13)
+                                                                                            (x14+y14)
+                                                                                            (x15+y15)
+                                                                                            (x16+y16)
+                                                                                            (x17+y17)
+                                                                                            (x18+y18)
+                                                                                            (x19+y19)
+                                                                                            (M.unionWith (+) x20 y20)
+                                                                                            (x21+y21)
+                                                                                            (x22+y22)
+                                                                                            (M.unionWith (+) x23 y23)
 
 
 instance NFData Analysis where
-    rnf (Analysis a1 a2 a3 a4 a5 a6 a7) = a1
+    rnf (Analysis a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23) = a1
                                    `deepseq` a2
                                    `deepseq` a3
                                    `deepseq` a4
                                    `deepseq` a5
                                    `deepseq` a6
                                    `deepseq` a7
+                                   `deepseq` a8
+                                   `deepseq` a9
+                                   `deepseq` a10
+                                   `deepseq` a11
+                                   `deepseq` a12
+                                   `deepseq` a13
+                                   `deepseq` a14
+                                   `deepseq` a15
+                                   `deepseq` a16
+                                   `deepseq` a17
+                                   `deepseq` a18
+                                   `deepseq` a19
+                                   `deepseq` a20
+                                   `deepseq` a21
+                                   `deepseq` a22
+                                   `deepseq` a23
                                    `deepseq` ()
 
 
